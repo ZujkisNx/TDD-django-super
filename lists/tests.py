a5e7bba6af05bfd0928356614aeaf2c6e6128e95
ZujkisNx <pauliusruzgas@gmail.com>
2014-06-14 21:50:47 +0300

    Showing one list item.

--- lists/tests.py
+++ lists/tests.py
@@ -20,3 +20,17 @@ class HomePageTest(TestCase):
         # into a Python unicode string, which allows us to compare
         # strings with strings, instad of bytes with bytes.
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
